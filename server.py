eb6312996b840871fc2f06be6c1917ac337cc75c
Bruno Paiva <brunopaiva@aluno.puc-rio.br>
2026-05-02 03:08:25 -0300

    Atualizando e comentando
    Atualizei o codigo para uma maneira mais sutil e comentei as linhas, de maneira que possa informar o que cada parte executa em meu código.

--- server.py
+++ server.py
@@ -1,37 +1,45 @@
+# Importa o Flask para criar o server
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+# Cria o app
 app = Flask(__name__)
 CORS(app)
 
+# Cria um BD (Inicialmente vazio)
 storage = {}
 
+# Define a rota '/document' que aceita o POST
 @app.route("/document", methods=["POST"])
 def document():
+
+    # Pega os dados enviados em JSON
     data = request.get_json()
 
+    # Retornar erro em modelo JSON, caso não haja dado
     if not data:
         return jsonify({"status": "error", "code": 400})
 
+    # Extrai as informações do nosso JSON
     action = data.get("action")
     key = data.get("key")
     value = data.get("value")
 
-    if not action or not key:
-        return jsonify({"status": "error", "code": 401})
-
+    # Faz a criação e retorna em JSON 
     if action == "create":
         if key in storage:
             return jsonify({"status": "error", "code": 1})
         storage[key] = value
         return jsonify({"status": "ok", "code": 0})
-
+        
+    # Faz a atualização e retorna em JSON 
     if action == "update":
         if key not in storage:
             return jsonify({"status": "error", "code": 2})
         storage[key] = value
         return jsonify({"status": "ok", "code": 0})
 
+    # Faz a remoção e retorna em JSON, apenas se a key existir ali no nosso BD...
     if action == "delete":
         if key not in storage:
             return jsonify({"status": "error", "code": 2})
